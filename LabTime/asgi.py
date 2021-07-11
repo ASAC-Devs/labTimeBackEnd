297dada9adb17640d31526fd7cd7802da867d952
RaneemAbdulazez <raneem97su@gmail.com>
2021-07-11 17:18:14 +0300

    startproject

---
+++ LabTime/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for LabTime project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LabTime.settings')
+
+application = get_asgi_application()
